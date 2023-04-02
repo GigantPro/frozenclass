03329274820b30331dc524d944e7b5a878e60602
GigantPro <pochtagigantpro@gmail.com>
2023-04-02 22:17:18 +0300

    Add poetry command: autoclean

---
+++ scripts/autoclean.py
@@ -0,0 +1,9 @@
+from os import system
+
+
+def autoclean():
+    system("rm -rf dist")
+    system("rm -rf frozenclass.egg-info")
+    system("rm -rf .pytest_cache")
+    system("rm -rf saves")
+    system("rm -rf */__pycache__")
