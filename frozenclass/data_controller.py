9dfd9599a734c09fd916e1b52ff3f54314f6ac3b
GigantPro <pochtagigantpro@gmail.com>
2023-04-02 09:21:12 +0300

    Add main docstrings

--- frozenclass/data_controller.py
+++ frozenclass/data_controller.py
@@ -6,12 +6,23 @@ from .dataparser import DataParser, DataWriter
 
 
 class DataController:
+    """The main class of the library. Responsible for its functions."""
     def __init__(self, saves_folder_path: str) -> None:
+        """__init__ method
+
+        Args:
+            saves_folder_path (str): Path to the folder where you plan to store/load classes
+        """
         self._saves_path = saves_folder_path
 
         os.makedirs(self._saves_path, exist_ok=True)
 
-    def get_all_saves_list(self) -> list[Any]:
+    def get_all_saves(self) -> list[Any]:
+        """Will return you instances of all previously saved classes.
+
+        Returns:
+            list[Any]: List of class instances.
+        """
         list_dir = os.listdir(self._saves_path)
         classes_list = []
 
@@ -21,6 +32,13 @@ class DataController:
         return classes_list
 
     def change_saves_path(self, new_path: str, copy_old: bool = False) -> None:
+        """Changes the path where the saves are stored, and, if necessary, moves the old saves there.
+Be careful, the next time you create a DataController, you will need to specify a new class path.
+
+        Args:
+            new_path (str): New save folder path;
+            copy_old (bool, optional): New save folder path. Defaults to False.
+        """
         os.makedirs(new_path, exist_ok=True)
 
         if copy_old:
@@ -33,7 +51,16 @@ class DataController:
 
     def freeze_class(
         self, target_class: Any, save_name: str = ...
-    ) -> bool | NoReturn:  # type(...) == ellipsis
+    ) -> bool:
+        """Used to save a class to a file
+
+        Args:
+            target_class (Any): The class to be saved
+            save_name (str, optional): The key by which it will be possible to obtain the saved class in the future. It is also displayed in the name of the file with saving. Defaults to ....
+
+        Returns:
+            bool: Whether the class was successfully saved to a file
+        """
         if isinstance(save_name, type(...)):
             if hasattr(target_class, '__name__'):
                 save_name = getattr(target_class, '__name__')
@@ -43,4 +70,12 @@ class DataController:
         return DataWriter(self._saves_path, save_name).freeze_class(target_class)
 
     def load_save(self, save_name: str) -> Any:
+        """Needed to load a save with a specific save name
+
+        Args:
+            save_name (str): The name of the desired save
+
+        Returns:
+            Any: The instance of the class that was saved
+        """
         pass
