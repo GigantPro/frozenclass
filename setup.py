3d49e0042667d73692bb22e4ea09b1bc35374b9a
GigantPro <pochtagigantpro@gmail.com>
2023-04-03 23:26:04 +0300

    Update python classifiers

--- setup.py
+++ setup.py
@@ -33,7 +33,11 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
