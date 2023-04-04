0c3705d76232ef568a9baabe62be476fcbb9f1f8
GigantPro <pochtagigantpro@gmail.com>
2023-04-04 13:54:23 +0300

    Fix linting

--- frozenclass/dataparser/data_parser.py
+++ frozenclass/dataparser/data_parser.py
@@ -61,7 +61,7 @@ class DataParser:
             if var["var_type"] in JSON_FORMATS:
                 _new_var_["var_value"] = json.loads(
                     _new_var_["var_value"]
-                )  
+                )
             new_vars.append(_new_var_)
 
         saved_data["var"] = new_vars
