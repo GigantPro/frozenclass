d2d659cb3736b596bf7706bed5c061e36ec43685
GigantPro <pochtagigantpro@gmail.com>
2023-04-12 18:58:33 +0300

    Add tests for autosave decorator

---
+++ tests/test_autosave.py
@@ -0,0 +1,39 @@
+from random import choice
+from string import ascii_letters
+import pytest
+
+from frozenclass import AutoFreeze, DataController
+
+
+@AutoFreeze('test_saves')
+class PublicClass:
+    __name__ = 'Test with public'
+    fst = 123
+    sec = '123123'
+
+
+def test_autosave_secorator_with_public_class():
+    test_object = PublicClass()
+    test_object.fst = 10
+
+    controller = DataController('test_saves')
+    loaded_class = controller.load_save('Test with public')
+
+    assert test_object.fst == loaded_class.fst and test_object.sec == loaded_class.sec
+
+
+def test_autosave_secorator_with_local_class():
+    @AutoFreeze('test_saves')
+    class LocalClass:
+        __name__ = 'Test with local'
+        fst = 123
+        sec = '123123'
+
+
+    test_object = LocalClass()
+    test_object.fst = 10
+
+    controller = DataController('test_saves')
+    loaded_class = controller.load_save('Test with local')
+
+    assert test_object.fst == loaded_class.fst and test_object.sec == loaded_class.sec
